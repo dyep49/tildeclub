8f7906131eceec8c9a989725c4de05904531b5bc
dyep49 <dyep49@gmail.com>
2014-10-09 18:06:21 -0400

    filters out empty messages

--- js/angular/controllers/home.js
+++ js/angular/controllers/home.js
@@ -2,7 +2,8 @@ define(['app', 'scribeInput', 'toTrusted'], function(app) {
 
   app.controller('HomeController', ['$scope', '$sce', '$http', function($scope, $sce, $http) {
 
-    var bucket = 'dillytilde'
+    // var bucket = 'dillytilde'
+    var bucket = 'testBucket111'
 
     $scope.message = {
       text: ''
@@ -17,6 +18,7 @@ define(['app', 'scribeInput', 'toTrusted'], function(app) {
     }
 
     function postMessage(message) {
+      debugger;
       message = angular.toJson(message);
       var url = 'http://tilde-club-db.appspot.com/put/buckets/' + bucket + '/' + message + '?callback=JSON_CALLBACK'
       $http.jsonp(url).success(function(response) {
@@ -29,6 +31,10 @@ define(['app', 'scribeInput', 'toTrusted'], function(app) {
       $http.jsonp(url).success(function(messages) {
         $scope.messages = messages;
       })
+    }
+
+    $scope.emptyFilter = function(message) {
+      return message.text !== ''
     }
 
     fetchMessages();
