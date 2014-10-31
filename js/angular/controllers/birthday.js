e97aace89c17ebc80f370a83f4b9bcb4fbc06fdd
dyep49 <dyep49@gmail.com>
2014-10-30 22:46:03 -0400

    updated birthday template

--- js/angular/controllers/birthday.js
+++ js/angular/controllers/birthday.js
@@ -1,9 +1,18 @@
 define(['app'], function(app) {
 
-  app.controller('BirthdayController', ['$scope', function($scope) {
+  app.controller('BirthdayController', ['$scope', '$routeParams', function($scope, $routeParams) {
 
-    $scope.name = "Toph";
+    $scope.name = $routeParams.name
 
+    var setImage = function() {
+      if ($routeParams.name === "Brittany") {
+        $scope.image = 'https://s3-us-west-2.amazonaws.com/misc-stuff-idk/532560_3143688915868_436974211_n.jpg'
+      } else {
+        $scope.image = 'https://s3-us-west-2.amazonaws.com/misc-stuff-idk/10495569_10202513871148054_6635838772700690405_o.jpg'
+      }
+    }
+
+    setImage();
 
   }])
 })
